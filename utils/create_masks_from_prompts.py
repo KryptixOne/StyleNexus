087fd96b3679cc159c16dabfb3b1a168d7303006
Daniel <85156415+KryptixOne@users.noreply.github.com>
2023-07-21 12:23:34 +0200

    SAM segmenation Mask and Holes Filling

--- utils/create_masks_from_prompts.py
+++ utils/create_masks_from_prompts.py
@@ -24,7 +24,7 @@ def create_all_mask_sam(img):
     checkpoin_path = r'D:\Ecommerce_FakeModel\SAM_Checkpoint\sam_vit_h_4b8939.pth'
     sam = sam_model_registry["vit_h"](checkpoint=checkpoin_path)
     sam.to('cuda')
-    mask_generator = SamAutomaticMaskGenerator(sam)
+    mask_generator = SamAutomaticMaskGenerator(sam, points_per_batch=128)
     masks = mask_generator.generate(img)
     return masks
 
@@ -47,32 +47,25 @@ def clip_scoring(image_bbox:list, prompt:str):
     with torch.no_grad():
         image_features = model.encode_image(stacked_images)
         text_features = model.encode_text(text)
-
+        # zero shot prediction
         image_features /= image_features.norm(dim=-1, keepdim=True)
         text_features /= text_features.norm(dim=-1, keepdim=True)
         similarity = (100.0 * text_features @ image_features.T).softmax(dim=-1)
         values, indices = similarity[0].topk(5)
 
-        #logits_per_image, logits_per_text = model(stacked_images, text)
-        #probs = logits_per_text.softmax(dim=-1).cpu().numpy()
 
     print("Label probs:", similarity)
-    return similarity
+    return similarity, values, indices
 def convert_back_to_pil(img_tensor):
     import numpy as np
     from PIL import Image
-
-    # Step 1: Convert to a NumPy array
+    # Convert to a NumPy array
     T_np = img_tensor.squeeze().cpu().numpy() if isinstance(img_tensor, torch.Tensor) else img_tensor.squeeze()
-
-    # Step 2: Reshape to [3, 256, 256]
     T_np = T_np.transpose(1, 2, 0)
-
-    # Step 3: Ensure values are in the [0, 255] range
+    # Ensure values are in the [0, 255] range
     T_np = np.clip(T_np, 0, 1) * 255.0
     T_np = T_np.astype(np.uint8)
-
-    # Step 4: Convert to a PIL image
+    # Convert to a PIL image
     pil_image = Image.fromarray(T_np)
 
     return pil_image
@@ -93,19 +86,44 @@ def build_boundingbox_image_stack_from_masks(masks:list, original_img):
 
     return img_bboxes
 
+def close_mask_holes(mask):
+    mask = mask.astype(np.uint8) * 255
+    # Define a large kernel for closing holes
+    kernel = np.ones((5, 5), np.uint8)
+    # Perform morphological closing to fill large holes
+    closed_mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+
+    contours, _ = cv2.findContours(closed_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Create an empty mask
+    filled_mask = np.zeros_like(closed_mask)
+
+    # Draw the filled contour on the mask
+    cv2.drawContours(filled_mask, contours, -1, (255), thickness=cv2.FILLED)
+
+    return closed_mask, filled_mask
+
+
 
 if __name__ == "__main__":
-    img_path = r'D:\Ecommerce_FakeModel\DreamboothTut\Doggy\alvan-nee-9M0tSjb-cpA-unsplash.jpeg'
+    img_path = r'D:\ArtDesigns\Forselling\GirlWearingLion.PNG'
     img = cv2.imread(img_path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     orig_image_pil = Image.fromarray(img)
 
-    prompt = 'a photo of a dog'
+    prompt = 'a photo of only a graphic T-Shirt'
     masks = create_all_mask_sam(img)
 
-    print()
     img_bboxes = build_boundingbox_image_stack_from_masks(masks,orig_image_pil)
-    probabilities = clip_scoring(img_bboxes, prompt)
+    probabilities, values, indices = clip_scoring(img_bboxes, prompt)
+
+    outmask_holes_filled, outmask_contour_filled = close_mask_holes(masks[indices[0]]['segmentation'])
+
+    plt.imshow(outmask_holes_filled)
+    plt.show()
+
+    plt.imshow(outmask_contour_filled)
+    plt.show()
 
     print()
 
