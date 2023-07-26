51a3cabdc79b5172b53386143df7757c020cbfe9
Daniel <85156415+KryptixOne@users.noreply.github.com>
2023-07-26 18:56:19 +0200

    enabled image upload and built generation button

--- webApp/webApp.py
+++ webApp/webApp.py
@@ -1,5 +1,7 @@
 import streamlit as st
-from passlib.hash import pbkdf2_sha256
+from PIL import Image
+from io import BytesIO
+from passlib.hash import sha256_crypt
 
 # Fake database (SQLite can be used in a real application)
 # For this example, we'll use a dictionary to store user information
@@ -7,22 +9,64 @@ from passlib.hash import pbkdf2_sha256
 users = {
     "john": {
         "username": "Daniel",
-        "password_hash": pbkdf2_sha256.hash("daniels_password"),
+        "password_hash": sha256_crypt.hash("daniels_password"),
     },
     "jane": {
         "username": "andy",
-        "password_hash": pbkdf2_sha256.hash("andys_password"),
+        "password_hash": sha256_crypt.hash("andys_password"),
     },
 }
 
 # Helper function to check if the provided password matches the stored hash
 def verify_password(password, password_hash):
-    return pbkdf2_sha256.verify(password, password_hash)
+    return sha256_crypt.verify(password, password_hash)
+
+# Function to perform ML model inference on the uploaded image
+def perform_inference(image):
+    # Your ML model inference code here
+    # Replace this with your actual ML model code
+    # For demonstration purposes, let's return a fixed example prediction
+    return "Prediction: Dress"  # Replace this with the actual model's output image
+
+# Function to downsample the image to a maximum resolution of 256x256
+def downsample_image(image):
+    max_size = (256, 256)
+    image.thumbnail(max_size, Image.LANCZOS)  # Use Image.LANCZOS instead of Image.ANTIALIAS
+    return image
 
 # Add login decorator to secure the "Inference" page
 def secure_inference_page():
     st.title('Model Inference Section')
     st.write('In this section, you can perform predictions using your trained ML model.')
+
+    # File uploader to allow users to upload images
+    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+    if uploaded_file is not None:
+        # Convert the uploaded file to PIL image
+        pil_image = Image.open(uploaded_file)
+
+        # Downsample the image
+        downsampled_image = downsample_image(pil_image)
+
+        # Display the input and output images side by side in two columns
+        col1, col2 = st.columns(2)
+        prediction = None
+
+        if st.button("Generate"):
+            prediction = perform_inference(pil_image)
+
+        with col1:
+            # Display the downsampled input image on the webpage
+            st.image(downsampled_image, caption="Downsampled Input Image", use_column_width=True)
+
+        with col2:
+            # Perform inference on the original resolution image
+                # Display the output of the ML model
+                st.image(pil_image, caption=prediction, use_column_width=True)
+
+    # Add empty space for the "Generate" button below the displayed images
+    st.empty()
 
 # Set page config
 st.set_page_config(page_title="StyleNexus Homepage", page_icon=':rocket:', layout='wide')
@@ -69,4 +113,4 @@ elif selected_page == 'Examples':
 
 # Footer
 st.markdown('---')
-st.write('Thank you for using the ML app. For more information, contact us at contact@example.com.')
+st.write('Thank you for using the ML app.')
