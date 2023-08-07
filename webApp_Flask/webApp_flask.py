204601cf61d734897edaacd6260c043c8a098fed
Daniel <85156415+KryptixOne@users.noreply.github.com>
2023-08-07 21:20:59 +0200

    updates to website, api call and in_painting function

--- webApp_Flask/webApp_flask.py
+++ webApp_Flask/webApp_flask.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, jsonify
-
+from PIL import Image
 from modules.api_calls import inpainting_api
-
+import cv2
+from matplotlib import pyplot as plt
 app = Flask(__name__)
 
 # Configuring static file route
@@ -9,21 +10,45 @@ app.static_folder = 'static'
 
 
 def generate_image(desired_prompt, segment_prompt, input_image):
-    checkpoint_directory_SD = None
-    checkpoint_path_SAM = None
+    checkpoint_directory_SD = r'./Models_Converted/Photon_inpaint'
+    checkpoint_path_SAM = r'./SAM_Checkpoint/sam_vit_h_4b8939.pth'
     lora_path = None
     border_mask_width = 8  # make it based off of image size
     negative_prompt = ('(worst quality, low quality, normal quality:2), bad quality, bad hands, ugly, NSFW'
                        )
-    height = None
-    width = None
+    # get init dimensions
+    image = Image.open(input_image)
 
+    max_dimension = 1024
+    image_width, image_height = image.size
+
+    # Calculate the new dimensions while maintaining the aspect ratio
+    aspect_ratio = image_width / image_height
+    new_width = min(image_width, max_dimension)
+    new_height = min(image_height, max_dimension)
+    if new_width / aspect_ratio > new_height:
+        new_width = int(new_height * aspect_ratio)
+    else:
+        new_height = int(new_width / aspect_ratio)
+
+    # Ensure new_width and new_height are divisible by 8
+    new_width = (new_width // 8) * 8
+    new_height = (new_height // 8) * 8
+
+    image = image.resize((new_width, new_height), Image.LANCZOS)
+
+    # Extract the dimensions after resizing
+    image_width, image_height = image.size
+    print(image_height, image_width)
+
+    # Now that we have extracted the dimensions, delete the 'image' variable to free up memory
+    del image
     generated_img = inpainting_api(prompt=desired_prompt,
                                    negative_prompt=negative_prompt,
                                    segmentation_prompt=segment_prompt,
                                    device='cuda',
                                    seeds=None,
-                                   height=height, width=width,
+                                   height=image_height, width=image_width,
                                    cfg_val=6,
                                    num_inference_steps=50,
                                    reference_image_path=input_image,
@@ -35,7 +60,7 @@ def generate_image(desired_prompt, segment_prompt, input_image):
                                    img2img_strength_second_pass_val=0.4)
 
     # You can process the image as needed and return the URL of the generated image.
-    generated_image_url = generated_img #"placeholder_image.jpg"
+    generated_image_url = generated_img[1]  #"placeholder_image.jpg"
     return generated_image_url
 
 
